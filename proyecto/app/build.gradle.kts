a3a4fbee6b4d50ca63720b4c98c86712073c9211
zeuzsamuel1829 <zeuzsamuel@gmail.com>
2026-08-30 08:14:18 -0500

    Update build.gradle.kts

--- proyecto/app/build.gradle.kts
+++ proyecto/app/build.gradle.kts
@@ -25,6 +25,7 @@ android {
         }
     }
 
+    // CORRECCIÓN: Java y Kotlin deben usar el mismo JVM target
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17

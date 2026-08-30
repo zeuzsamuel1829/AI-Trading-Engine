487af99227080a6233159c403908659622f5db8d
zeuzsamuel1829 <zeuzsamuel@gmail.com>
2026-08-30 09:05:56 -0500

    Update build.gradle.kts

--- proyecto/app/build.gradle.kts
+++ proyecto/app/build.gradle.kts
@@ -35,10 +35,6 @@ android {
     }
 }
 
-kotlin {
-    jvmToolchain(17)
-}
-
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")

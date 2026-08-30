85d1c65ff0a7a13a66cc9cd976bdc7381b8cdcd6
zeuzsamuel1829 <zeuzsamuel@gmail.com>
2026-08-30 08:33:25 -0500

    Update build.gradle.kts

--- proyecto/app/build.gradle.kts
+++ proyecto/app/build.gradle.kts
@@ -26,18 +26,17 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
